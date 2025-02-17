4b6bad646a3d1569bf33f1c3c71e0504af018a61
joannes0 <joannes1837@gmail.com>
2025-02-17 14:45:35 +0900

    오후 2시 45분

--- ex_0217/js/ex_p41-44.js
+++ ex_0217/js/ex_p41-44.js
@@ -3,26 +3,32 @@ box = 100;
 box = 30;
 document.write(box, "<br>"); //30
 //
-// var s = "javascript";
-// var num = "100";
-// var tag = "<h1>String</h1>";
-// //
-// var s = 100;
-// var t = Number("500"); //"500"->500
-// //
-// var s =true;
-// var t = 10 >= 100; //false
-// var k = Boolean("hello"); //true
-// //
-// var m = Boolean(0);
-// var k = Boolean("홍길동");
-// //
-// var a = true;
-// var b = false;
-// var c = 10 > 5;
-// var d = Boolean(null);
+var s = "javascript";
+var num = "100";
+var tag = "<h1>String</h1>";
+//
+var s = 100;
+var t = Number("500"); //"500"->500
+//
+var s = true;
+var t = 10 >= 100; //false
+var k = Boolean("hello"); //true
+//
+var m = Boolean(0);
+var k = Boolean("홍길동");
+//
+var a = true;
+var b = false;
+var c = 10 > 5;
+var d = Boolean(null);
 
-// document.write(a, "<br>");
-// document.write(b, "<br>");
-// document.write(c, "<br>");
-// document.write(d, "<br>");
+document.write(a, "<br>");
+document.write(b, "<br>");
+document.write(c, "<br>");
+document.write(d, "<br>");
+//
+var num = 100;
+var str = "자바스크립트";
+
+document.write(typeof num, "<br>");
+document.write(typeof str);
