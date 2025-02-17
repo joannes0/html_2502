181deb29951a2ae1c86ef6657edc8a4f99391ece
joannes0 <joannes1837@gmail.com>
2025-02-17 10:30:40 +0900

    오전 10시 30분

---
+++ ex_0217/js/ex_p41-44.js
@@ -0,0 +1,28 @@
+var box;
+box = 100;
+box = 30;
+document.write(box, "<br>"); //30
+//
+// var s = "javascript";
+// var num = "100";
+// var tag = "<h1>String</h1>";
+// //
+// var s = 100;
+// var t = Number("500"); //"500"->500
+// //
+// var s =true;
+// var t = 10 >= 100; //false
+// var k = Boolean("hello"); //true
+// //
+// var m = Boolean(0);
+// var k = Boolean("홍길동");
+// //
+// var a = true;
+// var b = false;
+// var c = 10 > 5;
+// var d = Boolean(null);
+
+// document.write(a, "<br>");
+// document.write(b, "<br>");
+// document.write(c, "<br>");
+// document.write(d, "<br>");
