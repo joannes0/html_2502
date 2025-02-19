04a79b917db2284394d7bd303a2e9979e883c226
joannes0 <joannes1837@gmail.com>
2025-02-19 17:22:17 +0900

    오후 5시 22분

--- ex_0219/js/index2_DOMEvents.js
+++ ex_0219/js/index2_DOMEvents.js
@@ -1,22 +1,49 @@
 function changeText(id) {
-    id.innerHTML = "Ooops!!!!!!!!"
+  id.innerHTML = "Ooops!!!!!!!!";
 }
 
 function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
+  document.getElementById("demo").innerHTML = Date();
 }
 
 document.getElementById("myBtn").onclick = displayDate2;
 function displayDate2() {
-    document.getElementById("demo2").innerHTML = Date();
+  document.getElementById("demo2").innerHTML = Date();
 }
 
 function checkCookies() {
-    let text = '';
-    if(navigator.cookieEnabled == true) {
-        text = 'Cookies are enabled.';
-    } else {
-        text = 'Cookies are not enabled.';
-    }
-    document.getElementById("demo3").innerHTML = text;
+  let text = "";
+  if (navigator.cookieEnabled == true) {
+    text = "Cookies are enabled.";
+  } else {
+    text = "Cookies are not enabled.";
+  }
+  document.getElementById("demo3").innerHTML = text;
+}
+
+function upperCaseOnChange() {
+  const x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
+
+function upperCaseOnInput() {
+  const x2 = document.getElementById("fname2");
+  x2.value = x2.value.toUpperCase();
+}
+
+function mOver(obj) {
+  obj.innerHTML = "Thank you";
+}
+function mOut(obj) {
+  obj.innerHTML = "Mouse Over Me";
+}
+
+
+function mDown(obj) {
+    obj.style.backgroundColor = " #1ec5e5";
+    obj.innerHTML = "Release Me";
+}
+function mUp(obj) {
+    obj.style.backgroundColor = "#d94a38";
+    obj.innerHTML = "Thank You";
 }
