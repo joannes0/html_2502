450408e356ec4d5f9e68dea4dfe7d7e01238183a
joannes0 <joannes1837@gmail.com>
2025-02-27 11:11:59 +0900

    오전 11시 11분

--- ex_0226/js/ex_p137.js
+++ ex_0226/js/ex_p137.js
@@ -1,14 +1,49 @@
 const elements = {
-  s2: { sec: document.querySelector(".s2") },
+  section_2: { section: document.querySelector(".s2") },
+  section_3: { div: document.querySelector(".s3 div") },
 };
 
-// winpopup.html과 연결되면 이미지도 뜸뜸
-function openPopup() {
-  if (elements.s2.sec) {
-    window.open(
-      "winpopup.html",
-      "pop1",
-      "width=300, height=400, left=300, top=50"
-    );
+// <p>태그 생성 함수. 단, 적용부 const div = ...로 선언해야함
+function createAndAppendP(div, text) {
+  const p = document.createElement("p");
+  p.textContent = text;
+  div.appendChild(p);
+}
+// // winpopup.html과 연결되면 이미지도 뜸
+// function openPopup() {
+//   if (elements.s2.section) {
+//     window.open(
+//       "winpopup.html",
+//       "pop1",
+//       "width=300, height=400, left=300, top=50"
+//     );
+//   }
+// }
+
+// setIterval()/clearInterval()
+let addNum = 0;
+let subNum = 1000;
+
+function updateDisplay(section) {
+  const div = elements[section].div;
+  if (div) {
+    createAndAppendP(div, `${section} 증가 값: ${addNum}`);
+    createAndAppendP(div, `${section} 감소 값: ${subNum}`);
   }
 }
+// 증가
+const auto_1 = setInterval(function () {
+  addNum++;
+  console.log('addNum: ' + addNum);
+  updateDisplay('section_3');
+}, 3000);
+
+// 감소
+const auto_2 = setInterval(function () {
+  subNum--;
+  console.log('subNum: ' + subNum);
+  updateDisplay('section_3');
+}, 3000);
+
+// 초기 값 표시
+updateDisplay('section_3');
